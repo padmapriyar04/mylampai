6e1338aa976fbd7209aae515a28d22477602ea86
ELECTROMIST <electromist.dev@gmail.com>
2024-07-27 17:14:14 +0530

    Added /questions

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { useEffect } from "react";
-import HeroSection from "../../components/home/HeroSection";
-import AboutWize from "../../components/home/AboutWize";
-import WizeCamp from "../../components/home/WizeCamp";
-import WhyWize from "../../components/home/WhyWize";
-import BackedByBest from "../../components/home/BackedByBest";
-import PowerOfWize from "../../components/home/PowerOfWize";
-import RecentAdvances from "../../components/home/RecentAdvances";
-import CommunitySection from "../../components/home/Community";
-import Footer from "../../components/home/Footer";
-import HomeSlider from "../../components/home/HomeSlider";
-import HomeNavbar from "../../components/home/HomeNavbar";
+// import HeroSection from "../../components/home/HeroSection";
+import HeroSection from "@/components/home/HeroSection";
+import AboutWize from "@/components/home/AboutWize";
+import WizeCamp from "@/components/home/WizeCamp";
+import WhyWize from "@/components/home/WhyWize";
+import BackedByBest from "@/components/home/BackedByBest";
+import PowerOfWize from "@/components/home/PowerOfWize";
+import RecentAdvances from "@/components/home/RecentAdvances";
+import CommunitySection from "@/components/home/Community";
+import Footer from "@/components/home/Footer";
+import HomeSlider from "@/components/home/HomeSlider";
+import HomeNavbar from "@/components/home/HomeNavbar";
 
 export default function Page() {
   useEffect(() => {
