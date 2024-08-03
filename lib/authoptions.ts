15d016600d666ff95ed7f71cc08b64751b8b8f58
arnavco9573 <arnavsharm589@gmail.com>
2024-08-03 21:01:35 +0530

    messages_media

--- lib/authoptions.ts
+++ lib/authoptions.ts
@@ -1,19 +1,49 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib";
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
-    debug: process.env.NODE_ENV === "development",
-    session: {
-        strategy: "jwt",
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google" && profile?.email) {
+        // Check if email exists
+        const existingUser = await prisma.user.findUnique({
+          where: { email: profile.email },
+        });
+
+        if (existingUser) {
+          // User already exists, do nothing or update some fields
+          console.log("User already exists:", existingUser);
+        } else {
+          // User does not exist, create a new user with the profile data
+          const newUser = await prisma.user.create({
+            data: {
+              name: profile.name || "",
+              email: profile.email,
+              image: profile.image || "",
+              role:"user",
+            },
+          });
+          console.log("User created:", newUser);
+        }
+      }
+
+      // Return a boolean or string to satisfy the Awaitable<string | boolean> return type
+      return true;
     },
-    secret: process.env.NEXTAUTH_SECRET as string,
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
 };
