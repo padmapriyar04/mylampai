2702d2382f7067a4ab3bdf6d53edb72f4b1d2c6a
ELECTROMIST <electromist.dev@gmail.com>
2024-08-06 14:32:59 +0530

    .

--- app/helpers/server.ts
+++ app/helpers/server.ts
@@ -1,10 +1,9 @@
 import prisma from "../../lib/index";
 
-
 export const connectToDatabase = async () => {
-    try {
-        await prisma.$connect();
-    } catch (error) {
-        throw new Error("Unable to connect to database")
-    }
-}
+  try {
+    await prisma.$connect();
+  } catch (error) {
+    console.error("Unable to connect to database:", error);
+  }
+};
