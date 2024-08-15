4ed07d71a5d218fb080c9bc30fba5e520c24456c
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-08-15 16:39:44 +0530

    .

--- components/home/PowerOfWize.tsx
+++ components/home/PowerOfWize.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import ImageGrid from "./ImageGrid";
 
 export default function PowerOfWize() {
-  
   return (
     <>
       <div className="flex flex-col  md:flex-row md:justify-between w-full max-w-[1300px] m-auto gap-4 ">
@@ -12,8 +11,9 @@ export default function PowerOfWize() {
           </p>
           <div className="flex flex-col items-start max-w-[500px] gap-8 pr-8 mb-4">
             <p className="text-3xl md:text-4xl font-medium">
-              Your <span className="text-[#B7B7B7]">Gateway</span> to a Dream Career - <span className="text-[#5FE8F1]">One</span>{" "}
-              platform, <br />a <span className="text-[#8C52FF]">Million</span> of
+              Your <span className="text-[#B7B7B7]">Gateway</span> to a Dream
+              Career - <span className="text-[#5FE8F1]">One</span> platform,{" "}
+              <br />a <span className="text-[#8C52FF]">Million</span> of
               Possibilities.
             </p>
             <button className="flex items-center gap-2 text-lg md:text-xl border-2 border-black rounded-full px-4 py-2 font-semibold hover:scale-[1.02] hover:bg-[#fafafa] transition-all duration-200">
@@ -31,34 +31,38 @@ export default function PowerOfWize() {
           </p>
         </div>
         <div className="grid gap-4 grid-cols-2 relative w-full max-w-[600px] m-auto p-4">
-          <Image
-            width={100}
-            height={100}
-            src="/home/PowerOfWize.svg"
-            alt="Background Image"
-            className="w-[298px] rounded-tr-[5rem] rounded-bl-[5rem] h-full"
-          />
-          <Image
-            width={100}
-            height={100}
-            src="/home/PowerOfWize.svg"
-            alt="Background Image"
-            className="w-[298px] rounded-br-[5rem] rounded-tl-[5rem]  h-full"
-          />
-          <Image
-            width={100}
-            height={100}
-            src="/home/PowerOfWize.svg"
-            alt="Background Image"
-            className="w-[298px] rounded-tl-[5rem] rounded-br-[5rem]  h-full"
-          />
-          <Image
-            width={100}
-            height={100}
-            src="/home/PowerOfWize.svg"
-            alt="Background Image"
-            className="w-[298px] rounded-bl-[5rem] rounded-tr-[5rem]  h-full"
-          />
+          <div className="relative bg-cover bg-[url('/home/PowerOfWize.svg')] md:bg-black w-full bg-center max-w-[290px] rounded-tr-[5rem] rounded-bl-[5rem] h-[276px]">
+            <div className="absolute flex items-center justify-center text-white w-[calc(100%-48px)] h-10 bg-[#ffffff60] backdrop-blur-lg top-6 left-4 rounded-3xl ">
+              types of design tools
+            </div>
+            <div className="absolute flex items-center justify-center text-3xl text-white font-medium w-[100px] h-16 bg-[#ffffff60] backdrop-blur-lg bottom-4 right-4 rounded-3xl ">
+              20+
+            </div>
+          </div>
+          <div className="relative bg-cover bg-[url('/home/PowerOfWize.svg')] md:bg-black w-full bg-center max-w-[290px] rounded-br-[5rem] rounded-tl-[5rem]  h-[276px]">
+            <div className="absolute flex items-center justify-center text-white w-[calc(100%-48px)] h-10 bg-[#ffffff60] backdrop-blur-lg top-6 right-4 rounded-3xl ">
+              types of design tools
+            </div>
+            <div className="absolute flex items-center justify-center text-3xl text-white font-medium w-[100px] h-16 bg-[#ffffff60] backdrop-blur-lg bottom-4 left-4 rounded-3xl ">
+              20+
+            </div>
+          </div>
+          <div className="relative bg-cover bg-[url('/home/PowerOfWize.svg')] md:bg-black w-full bg-center max-w-[290px] rounded-tl-[5rem] rounded-br-[5rem]  h-[276px]">
+            <div className="absolute flex items-center justify-center text-3xl text-white font-medium w-[100px] h-16 bg-[#ffffff60] backdrop-blur-lg top-4 right-4 rounded-3xl ">
+              20+
+            </div>
+            <div className="absolute flex items-center justify-center text-white w-[calc(100%-48px)] h-10 bg-[#ffffff60] backdrop-blur-lg bottom-6 left-4 rounded-3xl ">
+              types of design tools
+            </div>
+          </div>
+          <div className="relative bg-cover bg-[url('/home/PowerOfWize.svg')] md:bg-black w-full bg-center max-w-[290px] rounded-bl-[5rem] rounded-tr-[5rem]  h-[276px]">
+            <div className="absolute flex items-center justify-center text-3xl text-white font-medium w-[100px] h-16 bg-[#ffffff60] backdrop-blur-lg top-4 left-4 rounded-3xl ">
+              20+
+            </div>
+            <div className="absolute flex items-center justify-center text-white w-[calc(100%-48px)] h-10 bg-[#ffffff60] backdrop-blur-lg bottom-6 right-4 rounded-3xl ">
+              types of design tools
+            </div>
+          </div>
         </div>
         {/* <ImageGrid /> */}
       </div>
