d6ec2b3a8a6dc1e0985c6d8f1d37a028e62f7f52
kumar1397 <sahuiswar007@outlook.com>
2024-08-23 23:43:08 +0530

    1

--- app/(protected)/blogs/[blogId]/page.tsx
+++ app/(protected)/blogs/[blogId]/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaHome } from "react-icons/fa";
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 interface BlogPost {
     title: string;
