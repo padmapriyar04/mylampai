e200f5854f5baa9c81e8e6a2f5f51d51a5459b3d
kumar1397 <sahuiswar007@outlook.com>
2024-08-27 21:56:06 +0530

    changes

--- app/api/blogs/route.tsx
+++ app/api/blogs/route.tsx
@@ -4,19 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from 'jsonwebtoken';
 
 export const GET = async (request: NextRequest, res: NextResponse) => {
-  const authHeader = request.headers.get('authorization');
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return NextResponse.json({ message: "Authorization header missing or malformed" }, { status: 401 });
-  }
-  const token = authHeader.split(' ')[1];
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as { role: string };
-
-    // Check if the user has admin privileges
-    if (decodedToken.role !== 'admin') {
-      return NextResponse.json({ message: "Forbidden: Admins only" }, { status: 403 });
-    }
-
     const posts = await prisma.blog.findMany({
       include: {
         sections: true,
@@ -24,9 +12,6 @@ export const GET = async (request: NextRequest, res: NextResponse) => {
     });
     return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
-    if (err instanceof jwt.JsonWebTokenError) {
-      return NextResponse.json({ message: "Unauthorized: Invalid token" }, { status: 401 });
-    }
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 };
