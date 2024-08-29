8be999364902ac01e6ebae01259c917b7c0a5681
Aksh10x <akshathsurwase@gmail.com>
2024-08-29 22:37:23 +0530

    ui

--- components/navbar/NavItems.tsx
+++ components/navbar/NavItems.tsx
@@ -8,11 +8,11 @@ export default function NavItems(props: any) {
 
   return (
     <div
-      className={`flex flex-row px-5 py-3 w-full gap-4 grayscale hover:grayscale-0 relative duration-300 items-center group rounded-r-md transition ${
-        pathname == navData[props.index].Link ? "grayscale-0 text-primary shadow-xl border-y-2 border-r-2 border-purple-300 bg-primary-foreground" : ""
+      className={`flex flex-row px-5 py-2 w-full hover:bg-[#8c52ff10] gap-4 grayscale hover:grayscale-0 relative duration-300 items-center group rounded-r-md transition ${
+         pathname == navData[props.index].Link ? "grayscale-0 text-primary" : ""
       }`}
     >
-      <div className="h-4 left-0 bottom-1/2 -translate-y-1/2 absolute">
+      <div className="h-12 left-[-2px] bottom-1/2 translate-y-1/2 absolute">
         <Image
           src="/sidebar/navbarslider.svg"
           alt="slider"
@@ -20,7 +20,7 @@ export default function NavItems(props: any) {
           width={10}
           className={`${
             pathname == navData[props.index].Link ? "block" : "hidden"
-          } w-full`}
+          } h-full`}
         />
       </div>
       <div className="w-6 flex items-center">
