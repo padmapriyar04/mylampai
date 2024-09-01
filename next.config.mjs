dff2a9b3b5e60b238920a64540a562207bb2e2f6
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-09-02 03:20:17 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ["http://localhost:3000", "http://localhost:8000"],
     remotePatterns: [
       {
         protocol: "https",
