a66b319237bb8f71913cac9604eecbb2be8a9a31
Rust <ashutoshkrsingh.iitkgp@gmail.com>
2024-10-13 17:23:59 +0530

    protected layout fixed

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,15 +1,10 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import jwt from "jsonwebtoken";
-import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/global/Navbar";
 import type { Metadata } from "next";
-import "../globals.css";
-import { Open_Sans } from "next/font/google";
-import AuthProvider from "@/components/auth/AuthProvider";
 import Flexsidebar from "@/components/misc/Flexsidebar";
 
-const openSans = Open_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "MyLampAi",
@@ -37,17 +32,12 @@ export default function ProtectedLayout({
   // }
 
   return (
-    <html lang="en" className="scroll-smooth focus:scroll-auto">
-      <body className={`${openSans.className} bg-primary-foreground`}>
-        <AuthProvider>
-          <Navbar />
-          <div className="flex w-full h-full transition-all duration-300">
-            <Flexsidebar />
-            {children}
-          </div>
-          <Toaster position="top-center" />
-        </AuthProvider>
-      </body>
-    </html>
+    <>
+      <Navbar />
+      <div className="flex w-full h-full transition-all duration-300">
+        <Flexsidebar />
+        {children}
+      </div>
+    </>
   );
 }
