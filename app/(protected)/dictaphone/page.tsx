3a7f802d7448985a6e43c263e3bc2398d517032d
Rust <ashutoshkrsingh.iitkgp@gmail.com>
2024-11-04 21:14:42 +0530

    .

---
+++ app/(protected)/dictaphone/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import "regenerator-runtime";
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    // browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const startListening = () => {
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  };
+
+  return (
+    <>
+      <div>
+        <p>Microphone: {listening ? "on" : "off"}</p>
+        <button onClick={startListening}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+        <p>{transcript}</p>
+      </div>
+    </>
+  );
+};
+export default Dictaphone;
