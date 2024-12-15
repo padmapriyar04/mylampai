b50a7a4228fe70d2117b16adee3f5293646e3ffe
Chandaluri-Sathwik <saisathwik005@gmail.com>
2024-12-15 22:38:36 +0530

    added sidebar bottom navbar and recruiter route

--- app/(protected)/recruiter/layout.tsx
+++ app/(protected)/recruiter/layout.tsx
@@ -1,5 +1,7 @@
 import HomeNavbar from "@/components/home/HomeNavbar";
 import Footer from "@/components/global/Footer";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import BottomNavBar from "@/components/home/BottomNavBar";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,9 +12,8 @@ export default function RootLayout({
       <HomeNavbar />
       <div className="min-h-screen w-full flex flex-1 flex-col">
         {children}
-        <Footer />
+        <BottomNavBar/>
       </div>
-   
     </>
   );
 }
